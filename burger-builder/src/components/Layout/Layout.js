5e0f878bbb836f3a7c0c6f96066414c0883cbe78
Eduardo Zenardi <zee8ca@bosch.com>
2018-07-31 16:20:59 -0300

    add side drawer toggle button

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppAuxi from '../../hoc/AppAuxi';
 import classes from './Layout.css';
 import Toolbar from './../Navigation/Toolbar/Toolbar';
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer';
+import drawerToggle from './../Navigation/SideDrawer/DrawerToggle/DrawerToggle';
 
+class Layout extends Component {
 
-const layout = (props) => (
-    <AppAuxi>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}> {props.children} </main> 
-    </AppAuxi>
-);
+    state = {
+        showSideDrawer: true
+    }
 
-export default layout;
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler = () =>{
+        this.state((prevState) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        })
+    }
+
+    render() {
+        return (
+            <AppAuxi>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerCloseHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </AppAuxi>
+
+        );
+    }
+}
+
+export default Layout;
--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,15 @@ import React from 'react'
 import classes from './Toolbar.css';
 import Logo from './../../Logo/Logo';
 import NavigtionItems from './../NavigationItems/NavigationItems';
+import DrawerToggle from './../SideDrawer/DrawerToggle/DrawerToggle';
 
 //This is a funcional component. Dont need to
 //manage any state.
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
         <Logo height="80%" />
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigtionItems/>
         </nav>
     </header>
